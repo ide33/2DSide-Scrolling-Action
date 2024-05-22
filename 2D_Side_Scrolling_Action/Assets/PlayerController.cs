22335c7f4111a01b0364abfe49394eb15ea6d20c
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2024-05-22 12:29:33 +0900

    ブロック差し替え完了　左右移動スクリプト途中

--- 2D_Side_Scrolling_Action/Assets/PlayerController.cs
+++ 2D_Side_Scrolling_Action/Assets/PlayerController.cs
@@ -1,28 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-        void Start()
+    private Rigidbody rigidbody;
+    private float xSpeed;
+    void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
+        MoveUpdate ();
+    }
+    private void MoveUpdate()
+    {
         if(Input.GetKey (KeyCode.RightArrow))
         {
-            Vector2 pos = transform.position;
-            pos.x += 0.03f;
-            transform.position = pos;
+            xSpeed = 6.0f;
         }
         else if(Input.GetKey (KeyCode.LeftArrow))
         {
-            Vector2 pos = transform.position;
-            pos.x -= 0.03f;
-            transform.position = pos;
+           xSpeed = -6.0f;
+        }
+        else
+        {
+            xSpeed = 0.0f;
         }
     }
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rigidbody.velocity;
+        velocity.x = xSpeed;
+        rigidbody.velocity = velocity;
+    }
+
 }
